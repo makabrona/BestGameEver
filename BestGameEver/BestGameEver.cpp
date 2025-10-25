44c00b6ba46da77da2b90be23ed7d15c728fb66d
makabrona <105178677+makabrona@users.noreply.github.com>
2025-10-25 18:57:25 +0200

    first blueprint
    basic code

--- BestGameEver/BestGameEver.cpp
+++ BestGameEver/BestGameEver.cpp
@@ -1,8 +1,41 @@
-
 #include <iostream>
+#include <raylib.h>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+enum GameScreens { Main, Play, GameOver };
+GameScreens actualScreen = Play;
+
+// Variables
+float screenWidth = 800;
+float screenHeight = 800;
+
+int main() {
+
+	InitWindow(screenWidth, screenHeight, "BestGameEver");
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose()) {
+
+		BeginDrawing();
+		ClearBackground(BLACK);
+
+		switch (actualScreen) {
+		case Main:
+			break;
+		case Play:
+			break;
+		case GameOver:
+			break;
+		}
+
+		EndDrawing();
+
+	}
+
+	CloseWindow();
+	return 0;
 }
-
